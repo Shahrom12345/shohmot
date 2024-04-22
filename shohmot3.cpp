66a9c91861eb5cc46c66d4e045930e51fac4ae93
shahrom <167228749+Shahrom12345@users.noreply.github.com>
2024-04-22 14:02:52 +0500

    commit 3 na peremena

--- shohmot3.cpp
+++ shohmot3.cpp
@@ -6,19 +6,19 @@ int main()
 
  //VAZIR
 {
-  int x1,x2,y1,y2;
-  cin>>x1>>y1>>x2>>y2;
-  if(x1>8 || x2>8 || y1>8 || y2>8){
+  int x2,x4,y2,y4;
+  cin>>x2>>y2>>x4>>y4;
+  if(x2>8 || x4>8 || y2>8 || y4>8){
      cout<<"your must enter 1-8 if1";
   }
-  else if(x1<0 and x2<0 and y1<0 and y2<0){
+  else if(x2<0 and x4<0 and y2<0 and y4<0){
     cout<<"your must enter 1-8 if2";
   }
-   else if(x1==x2 || y1==y2 )
+   else if(x2==x4 || y2==y4 )
   {
       cout<<"Yes";
   }
-  else if(x1-y1 == x2-y2 || x1+y1 == x2+y2)
+  else if(x2-y2 == x4-y4 || x2+y2 == x4+y4)
      cout<<"Yes";
 
    else{
