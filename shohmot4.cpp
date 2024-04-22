b357186a5b472720fb8c33bbf4e290c9826f3ab5
shahrom <167228749+Shahrom12345@users.noreply.github.com>
2024-04-22 14:06:48 +0500

    commit 4 na peremena

--- shohmot4.cpp
+++ shohmot4.cpp
@@ -6,17 +6,17 @@ int main()
 
  //SHOH
 {
-  int x1,x2,y1,y2;
-  cin>>x1>>y1>>x2>>y2;
-  if(x1>8 || x2>8 || y1>8 || y2>8){
+  int x2,x4,y2,y4;
+  cin>>x2>>y2>>x4>>y4;
+  if(x2>8 || x4>8 || y2>8 || y4>8){
      cout<<"your must enter 1-8 if1";
   }
-  else if(x1<0 and x2<0 and y1<0 and y2<0){
+  else if(x2<0 and x4<0 and y2<0 and y4<0){
     cout<<"your must enter 1-8 if2";
   }
-   else if(abs(x1-x2)==0 && abs(y1-y2)==1 ||
-           abs(x1-x2)==1 && abs(y1-y2)==0 ||
-           abs(x1-x2)==1 && abs(y1-y2)==1 )
+   else if(abs(x2-x4)==0 && abs(y2-y4)==1 ||
+           abs(x2-x4)==1 && abs(y2-y4)==0 ||
+           abs(x2-x4)==1 && abs(y2-y4)==1 )
   {
       cout<<"Yes";
   }
